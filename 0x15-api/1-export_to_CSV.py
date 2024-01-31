1f615b83f26797fb6b4f59478bad577aa2acdd21
dakhamohammed <isanagy@live.fr>
2024-01-31 21:17:38 +0100

    Python script to export data in the CSV format.

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,17 +2,17 @@
 """records all tasks that are owned by this employee"""
 
 if __name__ == "__main__":
-    """records all tasks that are owned by this employee"""
     import requests
     import sys
     import csv
 
     fake_api = 'https://jsonplaceholder.typicode.com/'
-    employee = requests.get(fake_api + "users/{}".format(sys.argv[1])).json()
-    tasks = requests.get(fake_api + "todos", params={"userId": sys.argv[1]}).json()
+    _id = sys.argv[1]
+    employee = requests.get(fake_api + "users/{}".format(_id)).json()
+    tasks = requests.get(fake_api + "todos", params={"userId": _id}).json()
     employee_username = user.get("username")
     
-    with open("{}.csv".format(sys.argv[1]), "w", newline="") as csv_file:
+    with open("{}.csv".format(_id), "w", newline="") as csv_file:
     csv_w = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-    [csv_w.writerow([sys.argv[1], employee_username, task.get("completed"),
+    [csv_w.writerow([_id, employee_username, task.get("completed"),
                      task.get("title")]) for task in tasks]
