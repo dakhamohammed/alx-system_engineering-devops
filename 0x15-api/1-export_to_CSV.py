2955ee15fdb9ba4b636645e9831fa45fcdfeff2e
dakhamohammed <isanagy@live.fr>
2024-01-31 21:10:36 +0100

    Python script to export data in the CSV format.

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script to export data in the CSV format."""
+if __name__ == "__main__":
+    import requests
+    import sys
+    import csv
+
+    fake_api = 'https://jsonplaceholder.typicode.com/'
+    employee = requests.get(fake_api + "users/{}".format(sys.argv[1])).json()
+    tasks = requests.get(fake_api + "todos", params={"userId": sys.argv[1]}).json()
+    employee_username = user.get("username")
+    
+    with open("{}.csv".format(sys.argv[1]), "w", newline="") as csv_file:
+    csv_w = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+    [csv_w.writerow([sys.argv[1], employee_username, task.get("completed"),
+                     task.get("title")]) for task in tasks]
