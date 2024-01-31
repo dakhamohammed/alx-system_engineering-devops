b722a3cf2dd6484e206f06018a27ef1c62917bf7
dakhamohammed <isanagy@live.fr>
2024-02-01 00:15:57 +0100

    Python script to export data in the JSON format.

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     with open("todo_all_employees.json", "w") as json_file:
         json.dump({
             user.get("id"): [{"task": task.get("title"),
-                "completed": task.get("completed"),
-                "username": user.get("username")
-            } for task in requests.get(fake_api + "todos",
+                              "completed": task.get("completed"),
+                              "username": user.get("username")
+                              } for task in requests.get(fake_api + "todos",
                                        params={"userId": user.get("id")}
                                        ).json()]
             for user in users}, json_file)
