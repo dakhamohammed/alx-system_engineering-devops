a8b234abb1bf6bc42dfc6d2ac640559c52339a08
dakhamohammed <isanagy@live.fr>
2024-01-31 23:54:51 +0100

    export all tasks that are owned by this employee in the JSON format.

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,19 +2,19 @@
 """
 export all tasks that are owned by this employee in the JSON format.
 """
-if __name__ == "__main__":
-    import json
-    import requests
-    import sys
+import json
+import requests
+import sys
 
+if __name__ == "__main__":
     fake_api = 'https://jsonplaceholder.typicode.com/'
     _id = sys.argv[1]
     employee = requests.get(fake_api + "users/{}".format(_id)).json()
     tasks = requests.get(fake_api + "todos", params={"userId": _id}).json()
     employee_username = user.get("username")
 
-    with open("{}.json".format(_id), "w") as jsonfile:
+    with open("{}.json".format(_id), "w") as json_file:
         json.dump({_id: [{"task": task.get("title"),
                           "completed": task.get("completed"),
                           "username": employee_username
-                          } for task in tasks]}, jsonfile)
+                          } for task in tasks]}, json_file)
