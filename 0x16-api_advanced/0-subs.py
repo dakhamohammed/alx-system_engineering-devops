ef40923c5479942154d5e0797e6a9d76f68a1081
dakhamohammed <isanagy@live.fr>
2024-02-14 18:55:42 -0500

    Function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and returns the number of
-subscribers."""
+"""unction that queries the Reddit API and returns the number
+of subscribers."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-	"""returns the number of subscribers (not active users, total
-	subscribers), If an invalid subreddit is given return 0."""
-	api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	options = {
-		"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/dicor_)"
+	api = f'https://www.reddit.com/r/{subreddit}/about.json'
+	headers = {
+		"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/usr_)"
 	}
-	response = requests.get(api, headers=options, allow_redirects=False)
-	if response.status_code == 404:
+
+	res = requests.get(url, headers=headers, allow_redirects=False)
+	if res.status_code == 404:
 		return 0
-	results = response.json().get("data")
-	return results.get("subscribers")
+
+	res_returned = res.json().get('data')
+
+	return res_returned.get('subscribers')
