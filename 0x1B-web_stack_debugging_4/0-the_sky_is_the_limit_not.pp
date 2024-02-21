742745785eefa8b9f6e1b5179860bdb1994458d7
dakhamohammed <isanagy@live.fr>
2024-02-21 15:49:18 -0500

    fix huge amount of failed requests.

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# fix huge amount of failed requests.
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['sudo service nginx restart'],
+}
