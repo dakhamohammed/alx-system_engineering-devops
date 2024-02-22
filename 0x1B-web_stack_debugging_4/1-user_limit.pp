4c6c57fd5bfe7227f503eafb4700cda8e3c7dbfc
dakhamohammed <isanagy@live.fr>
2024-02-22 10:19:00 -0500

    Change the OS configuration so that it is possible to login with the holberton user.

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,7 +2,7 @@
 
 exec {'replace-after':
   provider => shell,
-  command  => 'sudo sed -i "s/nofile 10/nofile 100000/" /etc/security/limits.conf',
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
   before   => Exec['replace-2'],
 }
 
