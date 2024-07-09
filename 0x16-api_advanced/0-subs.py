f428231095ee3f62436d275d71747ada0111a1e0
Dakhamat Mohammed <isanagy@live.fr>
2024-07-09 19:02:22 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""unction that queries the Reddit API and returns the number
-of subscribers."""
+"""
+Queries the Reddit API and returns
+the number of subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers not active users but all
-    subscribers, if an invalide subreddit is given return 0."""
-    api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/usr_)"
-    }
-
-    res = requests.get(api, headers=headers, allow_redirects=False)
-
-    if res.status_code == 404:
+    headers = {'User-agent': 'dani random'}
+    URL = 'https://www.reddit.com/r/{}/about.json'
+    get_sudreddit = requests.get(URL.format(subreddit),
+                                 headers=headers, allow_redirects=False)
+    if get_sudreddit.status_code != 200:
         return 0
 
-    res_returned = res.json().get('data')
-
-    return res_returned.get('subscribers')
+    return get_sudreddit.json().get('data', {}).get('subscribers')
