47209a0cc444cc0111f0d01b4945aca8e82c602d
Dakhamat Mohammed <isanagy@live.fr>
2024-07-09 19:05:36 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""module that queries the Reddit API."""
+"""
+queries the Reddit API and prints
+the titles of the first 10 hot posts listed for a given subreddit.
+"""
+import pprint
 import requests
 
 
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed for a given
-    subreddit."""
-    api_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/lapbox_)"
-    }
-
-    params = {
-        "limit": 10
-    }
-
-    res = requests.get(api_url, headers=headers, params=params,
-                            allow_redirects=False)
-    if res.status_code == 404:
-        print("None")
-        return
-
-    res_returned = res.json().get("data")
-
-    [print(r.get("data").get("title")) for r in res_returned.get("children")]
+    headers = {'User-agent': 'dani random'}
+    URL = 'https://www.reddit.com/r/{}/hot.json'
+    get_sub = requests.get(URL.format(subreddit),
+                           params={"limit": 10, "g": "GLOBAL"},
+                           headers={"User-Agent": "Dani random"},
+                           allow_redirects=False)
+    data = get_sub.json().get('data', {}).get('children', {})
+    if get_sub.status_code != 200 or not data:
+        return print("None")
+    for post in data:
+        print(post.get('data', {}).get('title'))
